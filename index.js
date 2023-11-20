12a8595390cc144154835e1138b1c1d9108efd0c
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-20 08:53:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// string is a sequence of characters used to represent text
+
+// Create string
+let str = "Prince\tPanchal"
+console.log(str); // Prince
+
+// string length 
+console.log(str.length); // 14
+
+// String indices
+console.log(str[0]); // p
+
+// use of \n 
+console.log("Prince \nPanchal")
+
+// use of \t
+console.log("Prince \tPanchal")
+
+
